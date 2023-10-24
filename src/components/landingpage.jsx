3d36cdcece609958dbf30fc9bdbd6a3a5b3703e1
Eduardo Hidalgo <eduardohidalgogarcia0210@gmail.com>
2023-10-23 20:51:49 -0400

    theme changer done

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -10,7 +10,13 @@ export const LandingPage = () => {
     searchedCountry:'',
     selectedRegion:[],
     countries:[]
+
   })
+
+  
+  
+
+
 
   
   
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const NavBar = () => {
+
+
+  const [isDark, setIsDark] = useState(false)
+
+  const setDarkMode = () => {
+    setIsDark(true);
+    document.querySelector('body').setAttribute('data-theme','dark');
+    
+  }
+  
+  const setLightMode = () => {
+    setIsDark(false);
+    document.querySelector('body').setAttribute('data-theme','light');
+
+
+
+  }
+
+  const toggleTheme = () => {
+    isDark? setLightMode():setDarkMode()
+
+  }
+
+
+
   return (
     <>
      <nav className='navbar'>
         <h1 className='main_title'>Where in the world</h1>
         <div className='switch_theme'>
             {/* icono */}
-            <p>Dark Mode</p>
+            <p onClick={toggleTheme}>Dark Mode</p>
         </div>
     </nav>
     
