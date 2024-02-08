7b64ed9ba47e289083186738996abe4438fe61cb
Eduardo Hidalgo <eduardohidalgo0210@gmail.com>
2024-02-08 08:49:14 -0400

    dark mode fixed

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,41 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export const NavBar = () => {
+  const [colorScheme, setColorScheme] = useState('light');
 
-
-  const [isDark, setIsDark] = useState(false)
-
-  const setDarkMode = () => {
-    setIsDark(true);
-    document.querySelector('body').setAttribute('data-theme','dark');
-    
-  }
-  
-  const setLightMode = () => {
-    setIsDark(false);
-    document.querySelector('body').setAttribute('data-theme','light');
-
-
-
-  }
-
-  const toggleTheme = () => {
-    isDark? setLightMode():setDarkMode()
-
-  }
-
-
+  const changeColorScheme = () => {
+    const root = document.documentElement;
+    console.log(root)
+    if (colorScheme === 'light') {
+      root.setAttribute('data-color-scheme', 'dark');
+      setColorScheme('dark');
+    } else {
+      root.setAttribute('data-color-scheme', 'light');
+      setColorScheme('light');
+    }
+  };
 
   return (
     <>
-     <nav className='navbar'>
+      <nav className='navbar'>
         <h1 className='main_title'>Where in the world</h1>
         <div className='switch_theme'>
-            {/* icono */}
-            <p onClick={toggleTheme}>Dark Mode</p>
+          <p onClick={changeColorScheme}>Dark Mode</p>
         </div>
-    </nav>
-    
+      </nav>
     </>
-  )
-}
+  );
+};
+
